c222e62e48fb8e38d267190acbe31151ff5ec74b
BuildTools <unconfigured@null.spigotmc.org>
2025-12-03 01:44:26 -0300

    feat: adicionando componente MovieList e FieldSet

--- luz-e-cena/src/components/CardMovie/index.tsx
+++ luz-e-cena/src/components/CardMovie/index.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import styles from "./CardMovie.module.css";
 import Tag from "../Tag";
+import type { Movie } from "../../types";
 
-type Categoria = "2D" | "3D";
 
-type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
-
-interface CardMovieProps {
-  src: string;
-  alt: string;
-  titulo: string;
-  categoria: Categoria;
-  censura: Censura;
-  genero: string;
-  duracao: number;
-}
-
-const CardMovie = (props: CardMovieProps) => {
+const CardMovie = (props: Movie) => {
   const { alt, src, titulo, genero, categoria, censura, duracao } = props;
   return (
     <li className={styles.card}>
